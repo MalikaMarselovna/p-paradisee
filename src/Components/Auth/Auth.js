f74d76376ced2fb82266a18fcf34f327f6db6ee6
MalikaMarselovna <148754005+MalikaMarselovna@users.noreply.github.com>
2024-05-24 00:06:03 +0600

    test

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -8,11 +8,11 @@ export default function Auth() {
   const { user } = useContext(AppContext);
 
   return (
-    <div className="Auth">
-      {!user ? <button className="btn" onClick={LogIn}>Sign in</button> : null}
-      {user ? <span><Link to="/orders">{user.displayName}</Link></span> : null}
+    <div className="Auth flex">
+      {!user ? <button className="Sign-in_ic" onClick={LogIn}></button> : null}
+      {user ? <span className="text-[#f92e9e] text-[14px] flex items-center"><Link to="/orders">{user.displayName}</Link></span> : null}
       {user ? <button className="icon"><Link to="/orders"><i class="fa-solid fa-user" /></Link></button> : null}
-      {user ? <button className="btn" onClick={LogOut}>Sign out</button> : null}
+      {user ? <button className="Sign-out_ic" onClick={LogOut}></button> : null}
     </div>
   );
 }
--- src/Pages/ThankYou/ThankYou.js
+++ src/Pages/ThankYou/ThankYou.js
@@ -1,8 +1,8 @@
 export default function ThankYou() {
   return (
     <div className="ThankYou p-20">
-      <h1 className="flex justify-center items-center  text-[#f92e9e] text-5xl ">Thank You!</h1>
-      <p className="flex justify-center items-center  text-[#f92e9e] text-2xl mt-5"> Your order has been received.</p>
+      <h1 className="flex justify-center items-center text-center  text-[#f92e9e] text-5xl ">Thank You!</h1>
+      <p className="flex justify-center items-center text-center text-[#f92e9e] text-2xl mt-5"> Your order has been received.</p>
     </div>
   )
-}
+}  
