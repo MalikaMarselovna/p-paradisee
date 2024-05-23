c7c02f74213d6ae07033157691479cdbad2fb0b5
MalikaMarselovna <148754005+MalikaMarselovna@users.noreply.github.com>
2024-05-24 00:11:44 +0600

    make a fixed menu

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,24 +1,57 @@
 import "./Menu.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useEffect, useRef } from "react";
 
 function Menu() {
+  const location = useLocation();
+  const headerRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (headerRef.current) {
+        if (window.pageYOffset > headerRef.current.offsetTop) {
+          headerRef.current.classList.add("sticky");
+        } else {
+          headerRef.current.classList.remove("sticky");
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="Menu w-[100%] p-3 bg-[#f92e9e]">
-      <ul className="flex flex-1 items-center justify-around">
-        <li>
-          <Link to="/">Home</Link>
+    <nav ref={headerRef} id="myHeader" className="Menu w-[100%] p-3 bg-[#f92e9e]" aria-label="Main Menu">
+      <ul className="flex flex-1 items-center justify-around" role="none">
+        <li role="none">
+          <Link to="/" aria-current={location.pathname === "/" ? "page" : undefined}>
+            Home
+          </Link>
         </li>
-        <li>
-          <Link to="/about">About Us</Link>
+        <li role="none">
+          <Link to="/about" aria-current={location.pathname === "/about" ? "page" : undefined}>
+            About Us
+          </Link>
         </li>
-        <li>
-          <Link to="/categorypages">Our Products</Link>
+        <li role="none">
+          <Link to="/categorypages" aria-current={location.pathname === "/categorypages" ? "page" : undefined}>
+            Our Products
+          </Link>
         </li>
-        <li>
-          <Link to="/delivery">Delivery</Link>
+        <li role="none">
+          <Link to="/delivery" aria-current={location.pathname === "/delivery" ? "page" : undefined}>
+            Delivery
+          </Link>
         </li>
-        <li>
-          <Link to="/contacts">Contacts Us</Link>
+        <li role="none">
+          <Link to="/contacts" aria-current={location.pathname === "/contacts" ? "page" : undefined}>
+            Contact Us
+          </Link>
         </li>
       </ul>
     </nav>
@@ -26,3 +59,5 @@ function Menu() {
 }
 
 export default Menu;
+
+
