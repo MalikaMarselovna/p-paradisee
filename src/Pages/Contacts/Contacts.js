ed3eb5957df008969e81e9cc8bb5b0be587e5ea8
MalikaMarselovna <148754005+MalikaMarselovna@users.noreply.github.com>
2024-05-24 09:13:42 +0600

    fix contacts page

--- src/Pages/Contacts/Contacts.js
+++ src/Pages/Contacts/Contacts.js
@@ -1,88 +1,47 @@
-import "./Contacts.css";
 import React from "react";
-import {
-  Facebook,
-  Instagram,
-  LocationOnOutlined,
-  LocalPhoneOutlined,
-  Pinterest,
-  Twitter,
-  YouTube,
-  EmailOutlined,
-  WhatsApp,
-} from "@mui/icons-material";
+import { LocationOnOutlined, EmailOutlined, LocalPhoneOutlined } from "@mui/icons-material";
+import ContactForm from "../../Components/ContactForm/ContactForm";
+import "./Contacts.css";
+
+function ContactInfo() {
+  return (
+    <div className="contact-info">
+      <div className="contact-item">
+        <LocationOnOutlined className="icon" />
+        <span className="info-text">State of Karakol</span>
+      </div>
+      <div className="contact-item">
+        <EmailOutlined className="icon" />
+        <a href="mailto:mmukashova1@gmail.com" className="info-link">plushp@gmail.com</a>
+      </div>
+      <div className="contact-item">
+        <LocalPhoneOutlined className="icon" />
+        <a href="tel:+996704884698" className="info-link">+996-704-884-698</a>
+      </div>
+    </div>
+  );
+}
 
 function Contacts() {
   return (
-    <div className="flex flex-col items-center justify-center p-5">
-      <h1 className="text-5xl font-bold mb-4 text-[#f92e9e] text-center">
-        It Is Very Easy To Contact Us
-      </h1>
+    <div className="contacts-container">
+      <h1 className="main-title">It Is Very Easy To Contact Us</h1>
 
-      <div className="w-full">
-        <div className="ml-20 mt-20">
-          <p className="text-center text-3xl text-black mb-10">
-            Just call, write, or come to our office
-          </p>
-          <p className="text-center text-4xl text-[#16ffbd]">
-            Available daily from 9:00 to 18:00
-          </p>
+      <div className="info-section">
+        <div className="info-text">
+          <p className="info-description">Just call, write, or come to our office</p>
+          <p className="info-description">Available daily from 9:00 to 18:00</p>
         </div>
 
-        <div className="flex flex-col p-2 items-start p-10 h-[86vh]">
-          <h1 className="m-5 text-3xl font-bold mb-4 text-[#f92e9e]">
-            Contact Us
-          </h1>
-          <div className="flex m-3">
-            <LocationOnOutlined className="text-[#f92e9e]" />
-            <a
-              href="https://maps.app.goo.gl/jEnMYyCj8hDEcnic8"
-              className="pl-3 hover:text-[#16ffbd]"
-            >
-              State of Karakol
-            </a>
-          </div>
-          <div className="flex m-3">
-            <EmailOutlined className="text-[#f92e9e]" />
-            <a
-              href="mailto:mmukashova1@gmail.com"
-              className="pl-3 hover:text-[#16ffbd]"
-            >
-              plushp@gmail.com
-            </a>
-          </div>
-          <div className="flex m-3">
-            <LocalPhoneOutlined className="text-[#f92e9e]" />
-            <a href="tel:+996704884698" className="pl-3 hover:text-[#16ffbd]">
-              +996-704-884-698
-            </a>
-          </div>
-          <div className="flex m-3">
-            <WhatsApp className="text-[#f92e9e]" />
-            <a
-              href="https://wa.me/+996704884698"
-              className="pl-3 hover:text-[#16ffbd]"
-            >
-              +996-704-884-698
-            </a>
-          </div>
-        </div>
+        <ContactInfo />
       </div>
 
-      <div className="w-full mt-10">
-        <h1 className="text-5xl font-bold mb-4 text-[#f92e9e] text-center">
-          Our Location
-        </h1>
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2942.396313828574!2d78.38404677593776!3d42.48312647118172!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38865b5c92042535%3A0x78a9de24f2cc9c3b!2sIT-Academy%20Karakol!5e0!3m2!1sru!2skg!4v1715444746148!5m2!1sru!2skg"
-          className="w-full h-[500px] border-0 border-2 border-white rounded-lg"
-          allowFullScreen=""
-          loading="lazy"
-        ></iframe>
+      <div className="contact-form-section">
+        <h2 className="contact-title">Contact Us</h2>
+        <ContactForm />
       </div>
     </div>
   );
 }
 
 export default Contacts;
-
