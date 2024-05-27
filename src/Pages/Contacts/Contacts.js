658b31e0c24b3a9c8d67eb8aa27bc3cec8ef7af6
MalikaMarselovna <148754005+MalikaMarselovna@users.noreply.github.com>
2024-05-27 14:09:17 +0600

    change form

--- src/Pages/Contacts/Contacts.js
+++ src/Pages/Contacts/Contacts.js
@@ -77,16 +77,17 @@ function Contact() {
           
         </div>
       </div>
-      <div className='w-[100%] flex justify-between items-center'>
+      <div className='w-[100%] flex justify-center items-center'>
 
 
-       <ContactForm className="shadow-2xl"/>
+       <ContactForm className="100%"/>
 
-        <iframe 
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2942.396313828574!2d78.38404677593776!3d42.48312647118172!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38865b5c92042535%3A0x78a9de24f2cc9c3b!2sIT-Academy%20Karakol!5e0!3m2!1sru!2skg!4v1715444746148!5m2!1sru!2skg"
-          className="w-[45%] h-[500px] border-0 flex justify-center items-center border-2 border-[#fff] rounded-lg shadow-2xl"
-        ></iframe>
+       
       </div>
+      <iframe 
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2942.396313828574!2d78.38404677593776!3d42.48312647118172!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38865b5c92042535%3A0x78a9de24f2cc9c3b!2sIT-Academy%20Karakol!5e0!3m2!1sru!2skg!4v1715444746148!5m2!1sru!2skg"
+          className="w-[100%] h-[500px] border-0 flex justify-center items-center border-2 border-[#fff] rounded-lg shadow-2xl"
+        ></iframe>
 
 
 
