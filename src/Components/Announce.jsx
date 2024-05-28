38265488f922377e409b0aacb86ec97d1df16e3f
MalikaMarselovna <148754005+MalikaMarselovna@users.noreply.github.com>
2024-05-28 13:43:27 +0600

    test

--- src/Components/Announce.jsx
+++ src/Components/Announce.jsx
@@ -1,19 +1,23 @@
-import React, { useState } from 'react';
-import { Close } from '@mui/icons-material';
+import React, { useState } from "react";
+import { Close } from "@mui/icons-material";
 
 function Announce() {
-    const [announceStyle, setAnnounceStyle] = useState('bg-[#f92e9e] font-bold text-white flex items-center justify-center p-[10px]');
+  const [announceStyle, setAnnounceStyle] = useState(
+    "bg-[#f92e9e] font-bold text-white flex items-center justify-center p-[10px] container"
+  );
 
-    const handleClose = () => {
-        setAnnounceStyle(announceStyle + " hidden");
-    }
+  const handleClose = () => {
+    setAnnounceStyle(announceStyle + " hidden");
+  };
 
-    return (
-        <div className={announceStyle}>
-            <h2 className='container'> Hurry up it's 40% off now</h2>
-            <Close className='cursor-pointer ml-6' onClick={handleClose} />
-        </div>
-    )
+  return (
+    <div className="bg-[#f92e9e]">
+      <div className={announceStyle}>
+        <h2> Hurry up it's 40% off now</h2>
+        <Close className="cursor-pointer ml-6 " onClick={handleClose} />
+      </div>
+    </div>
+  );
 }
 
 export default Announce;
